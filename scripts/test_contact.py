9f13dbba9a00cb62b4d9fcb6024822def49a2bb5
273souffle <15811593542@163.com>
2019-03-08 11:19:13 +0800

    first commit

---
+++ scripts/test_contact.py
@@ -0,0 +1,23 @@
+import time
+
+import allure
+import pytest
+
+from base.base_analyze import analyze_file
+from base.base_driver import init_driver
+from page.page import Page
+
+
+class TestContact:
+
+    def test_contact1(self):
+        assert 0
+
+    def test_contact2(self):
+        assert 1
+
+    def test_contact3(self):
+        assert 1
+
+    def test_contact4(self):
+        assert 0
